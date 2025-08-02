99079005eb3a0f62f013140b5baefd726299da7c
rachel <rahelnaba716@gmail.com>
2025-08-02 14:41:44 -0700

    cpp

---
+++ sage3/c.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j,k;
+    for(i=1; i<=5; i++){
+    	cout<<"*"<<endl;
+	
+	
+    	{
+    		for(j=1; j<5-1; j++){
+    				cout<<" "<<endl;
+    			} for(k=1;k<=i;k++)
+				{
+				
+    			cout<<"*"<<" "<<endl;
+			}
+		}
+}
+  return 0;
+  }
